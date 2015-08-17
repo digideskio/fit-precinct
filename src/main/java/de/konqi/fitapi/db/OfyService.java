e387c0e81a5e26af3949eb95fc284985f497aa5a
konqi <elkonqi@gmail.com>
2015-08-17 20:44:55 +0200

    Implenented POST and GET Methods for openfitapi

--- src/main/java/de/konqi/fitapi/db/OfyService.java
+++ src/main/java/de/konqi/fitapi/db/OfyService.java
@@ -1,5 +1,6 @@
 package de.konqi.fitapi.db;
 
+import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyFactory;
 import com.googlecode.objectify.ObjectifyService;
 
@@ -7,17 +8,16 @@ import com.googlecode.objectify.ObjectifyService;
  * Created by konqi on 16.08.2015.
  */
 public class OfyService {
-    static ObjectifyFactory objectifyFactory;
     static {
-        objectifyFactory = new ObjectifyFactory();
-        objectifyFactory.register(Workout.class);
+        ObjectifyService.register(Workout.class);
+        ObjectifyService.register(WorkoutData.class);
     }
 
-    static ObjectifyService ofy() {
-        return new ObjectifyService();
+    public static Objectify ofy() {
+        return ObjectifyService.ofy();
     }
 
-    static ObjectifyFactory factory() {
-        return objectifyFactory;
+    public static ObjectifyFactory factory() {
+        return ObjectifyService.factory();
     }
 }
--- src/main/java/de/konqi/fitapi/db/Workout.java
+++ src/main/java/de/konqi/fitapi/db/Workout.java
@@ -1,9 +1,80 @@
 package de.konqi.fitapi.db;
 
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by konqi on 16.08.2015.
  */
+@Entity
 public class Workout {
-    
-    
+    @Id
+    private Long id = null;
+
+    Date startTime;
+    String type;
+    String sharing;
+    String name;
+    String notes;
+    Map<String, Double> data = new HashMap<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSharing() {
+        return sharing;
+    }
+
+    public void setSharing(String sharing) {
+        this.sharing = sharing;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Map<String, Double> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, Double> data) {
+        this.data = data;
+    }
 }
